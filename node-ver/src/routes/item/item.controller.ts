8c933327cb6ab829f001b433b6d993540c821fc9
MasonMS <brightcastle91@gmail.com>
2022-03-30 13:58:16 +0900

    ⚰️ [DEL] : 불필요 api 함수 삭제

--- node-ver/src/routes/item/item.controller.ts
+++ node-ver/src/routes/item/item.controller.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import mysql from 'mysql2';
-const router = express.Router()
 const config = require('@config/key');
 
 import { isEmpty } from '../../utils/helpers'
@@ -77,74 +76,6 @@ exports.getItems = (req: express.Request, res:express.Response) => {
   }
 }
 
-exports.getItemWithPrice = (req: express.Request, res:express.Response) => {
-  const { from: from_price, to: to_price } = req.query
-  try {
-    const sql = `
-      select item_name as ITEM_NAME,
-            item_price as ITEM_PRICE,
-            item_image as ITEM_IMAGE,
-            item_id as ITEM_ID
-      from item_info
-      where item_price BETWEEN '${from_price}' and '${to_price}';
-    `
-
-    connection.promise().query(sql)
-    .then( (result: any) => {
-      if (result[0].length === 0) {
-        return res.json(makeResponseFormat('0000', [], 0))
-      } else {
-        return res.json(makeResponseFormat('0000', result[0], result[0].length))
-      }
-    })
-    .catch((err) => {
-      console.log('[masonms] error: ', err)
-      connection.end()
-      return res.json(makeResponseFormat('9999', [], 0, err))
-    })
-    .then( () => {
-      console.log('[masonms] finally then')
-    });
-  } catch (error) {
-    connection.end()
-    return res.json(makeResponseFormat('9999', [], 0, error))
-  }
-}
-
-exports.getItemWithTitle = (req: express.Request, res:express.Response) => {
-  const { title: search_title } = req.query
-  try {
-    const sql = `
-      select item_name as ITEM_NAME,
-            item_price as ITEM_PRICE,
-            item_image as ITEM_IMAGE,
-            item_id as ITEM_ID
-      from item_info
-      where item_name like '%${search_title}%';
-    `
-
-    connection.promise().query(sql)
-    .then( (result: any) => {
-      if (result[0].length === 0) {
-        return res.json(makeResponseFormat('0000', [], 0))
-      } else {
-        return res.json(makeResponseFormat('0000', result[0], result[0].length))
-      }
-    })
-    .catch((err) => {
-      console.log('[masonms] error: ', err)
-      connection.end()
-      return res.json(makeResponseFormat('9999', [], 0, err))
-    })
-    .then( () => {
-      console.log('[masonms] finally then')
-    });
-  } catch (error) {
-    connection.end()
-    return res.json(makeResponseFormat('9999', [], 0, error))
-  }
-}
-
 exports.getCategoryData = (req: express.Request, res:express.Response) => {
   try {
     const sql = `
@@ -152,40 +83,6 @@ exports.getCategoryData = (req: express.Request, res:express.Response) => {
              code_name as CODE_NAME
       from base_code
       where code_id like 'CA%';
-    `
-
-    connection.promise().query(sql)
-    .then( (result: any) => {
-      if (result[0].length === 0) {
-        return res.json(makeResponseFormat('0000', [], 0))
-      } else {
-        return res.json(makeResponseFormat('0000', result[0], result[0].length))
-      }
-    })
-    .catch((err) => {
-      console.log('[masonms] error: ', err)
-      connection.end()
-      return res.json(makeResponseFormat('9999', [], 0, err))
-    })
-    .then( () => {
-      console.log('[masonms] finally then')
-    });
-  } catch (error) {
-    connection.end()
-    return res.json(makeResponseFormat('9999', [], 0, error))
-  }
-}
-
-exports.getItemWithCategory = (req: express.Request, res:express.Response) => {
-  const { category } = req.query
-  try {
-    const sql = `
-      select item_name as ITEM_NAME,
-            item_price as ITEM_PRICE,
-            item_image as ITEM_IMAGE,
-            item_id as ITEM_ID
-      from item_info
-      where item_category like '${category}%';
     `
 
     connection.promise().query(sql)
