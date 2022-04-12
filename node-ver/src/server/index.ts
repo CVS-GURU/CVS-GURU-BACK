b3d269ca8ab1bf044e120f25b9e513f2e5bed781
westkite1201 <westkite1201@gmail.com>
2022-04-12 12:14:53 +0900

    🔨 [FIX] :  CORS 설정 변경

--- node-ver/src/server/index.ts
+++ node-ver/src/server/index.ts
@@ -1,37 +1,34 @@
 import 'module-alias/register';
-import express from 'express'; 
+import express from 'express';
 
-const api = require('../routes')
+const api = require('../routes');
 
-const app = express(); 
-const port = 3031; 
+const app = express();
+const port = 3031;
 
-import cookieParser from 'cookie-parser'; 
-const cors = require("cors")({
-  origin: '*', // 출처 허용 옵션
-  credential: 'true' // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
-})
+import cookieParser from 'cookie-parser';
+const cors = require('cors');
 const config = require('@config/key');
 
-app.use(cors)
-app.use(express.json()); 
-app.use(express.urlencoded({extended : true }));
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api', api)
+app.use('/api', api);
 
-const { swaggerUi, specs } = require('../../middleware/Swagger')
+const { swaggerUi, specs } = require('../../middleware/Swagger');
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
-app.get('/', (req: express.Request, res: express.Response) => { 
-  res.send('Hello World'); 
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('Hello World');
 });
 
-app.post('/api/ping', (req: express.Request, res: express.Response) => { 
-  res.send('post test')  
+app.post('/api/ping', (req: express.Request, res: express.Response) => {
+  res.send('post test');
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port} and mode ${config.dataMode}`); 
-})
+  console.log(`app listening on port ${port} and mode ${config.dataMode}`);
+});
